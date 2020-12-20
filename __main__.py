a6ac5caecdd4e17000e7bea905c358a8b1f3cc1e
Evan Schwartzentruber <62043858+Daemitrious@users.noreply.github.com>
2020-12-19 23:51:48 -0500

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,111 +1,150 @@
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
 
+from requests import get, post
 from bs4 import BeautifulSoup
 
-from requests import get, post
-
-from os import mkdir
+from os import makedirs
 from os.path import exists
+
+from getpass import getuser
 
 from time import sleep
 
 
-options = Options()
-options.add_argument("--headless")
+def headless(options):  #  Set selenium options to headless mode
+    options.add_argument("--headless")
+    return options
+
+def status(content, bag=['']):  #  Print over initial print
+    bag.append(content)
+    print(str(content) + " " * len(bag[-2]), end="\r")
 
 
-H1 = "https://www12.9anime.to"
-H2 = "https://vidstream.pro/download/"
+#  Hosts
+HOST = "https://www12.9anime.to"
+VIDSTREAM = "https://vidstream.pro/download/"
 
+
+#  Headers required for post request
 HEADERS = {"Content-Type": "application/x-www-form-urlencoded"}
 
 
-def getsoup(html):  # Alias for BeautifulSoup
-    return BeautifulSoup(html, "lxml")
-
-
-def gethtml(url):  # requests.get -> soup
-    return getsoup(get(url).text)
-
-
-def getjs(url):  # selenium.get -> soup
-    driver = Firefox(options=options)
-    driver.get(url)
-    sleep(1)
-    soup = getsoup(driver.page_source)
-    driver.close()
-    return soup
-
-
-def grabkey(soup):
-    return soup.find("div", id="player").find("iframe")["src"].split("?")[0].rsplit("/", 1)[1]
-
-
-def grabdl(URL):
-    return post(URL, data={"token": gethtml(URL).find("input", type="hidden")["value"]}, headers=HEADERS, stream=True).url
-
-
-def findserver(url, title):
-    SOUP = gethtml(url + title.replace(" ", "+")).find("a", class_="name")
-
-    return SOUP["href"], SOUP["data-jtitle"] if SOUP else None
-
-
-def usr(content):
+def usr(content):  #  Manipulates user's input in order to grab anime and episodes(s)
     return [x.strip() for x in content.split(",")]
 
 
-def grabbing(EPISODES):  #  FIX - FIND NEXT EPISODE INSTEAD OF ADDING
-    for ep in EPISODES:
-        yield PATH % ep.rsplit("-", 1)[1], grabdl(H2 + grabkey(getjs(ep)))  #  ||| AFTER INITIAL FIX ||| FIX - DONT SPLIT | JUST USE ep
+def getsoup(html):  #  html  ->  BeautifulSoup
+    return BeautifulSoup(html, "lxml")
+
+def gethtml(url):  #  requests.get  ->  BeautifulSoup
+    return getsoup(get(url).text)
+
+def getjs(driver, urls):  #  webdriver.get  -> BeautifulSoup
+
+    for url in urls:
+        status(f"Scraping episode {url.rsplit('-', 1)[1]}...")
+        driver.get(url)
+        sleep(1)
+        yield getsoup(driver.page_source)
+
+    driver.quit()
 
 
-def check_path_ep(EPISODES):
+
+def grabkey(soup):  #  Grabs key for specified episode
+    return soup.find("div", id="player").find("iframe")["src"].split("?")[0].rsplit("/", 1)[1]
+
+
+def grabdl(URL):  #  Post request in order to grab downlink link from direct host
+    return post(URL, data={"token": gethtml(URL).find("input", type="hidden")["value"]}, headers=HEADERS, stream=True).url
+
+
+def findserver(url, title):  #  Finds title and server of anime
+
+    SOUP = gethtml(url + title.replace(" ", "+")).find("a", class_="name")
+    return SOUP["href"], SOUP["data-jtitle"] if SOUP else None
+
+
+def grabbing(EPISODES):  #  Grabs path and direct download link for each episode
+
+    for path, dl in zip([PATH % ep.rsplit("-", 1)[1] for ep in EPISODES], [grabdl(VIDSTREAM + grabkey(soup)) for soup in getjs(Firefox(options=headless(Options())), EPISODES)]):
+        yield path, dl
+
+
+
+def check_path_ep(EPISODES):  #  Checks if any of the episodes requested are already downloaded
+
     for ep in EPISODES:
         if not exists(PATH % ep):
             yield EP_FMT % ep
 
 
-def check_path_title(path):
+def check_path_title(path):  #  Checks if there is an existing directory of the requested anime
+
     if not exists(path):
-        mkdir(path)
+        makedirs(path)
+
     return path + "Ep %s.mp4"
 
 
-def download(INFO):
+
+def download(INFO):  #  Writes the content of each direct download link to the specified path.
+
     for path, url in INFO:
+
         with open(path, "wb") as f:
+            status("Downloading " + path[:-4].rsplit('\\', 1)[1])
+
             for chunk in get(url, stream=True).iter_content(1024):
                 f.write(chunk)
 
 
 
 
-ANIME, EPS = usr(input("\n\t> "))
+#  Path to where the anime is stored
+DOWNLOAD_PATH = "C:\\Users\\" + getuser() + "\\Videos\\Anime"
 
 
-print("Locating...")
-OUTPUT = findserver(H1 + "/filter?type[]=tv&language[]=subbed&keyword=", ANIME)
+
+ANIME, EPS = usr(input(f"{DOWNLOAD_PATH}\n\n\t> "))
 
 
-if OUTPUT:
-    print("Found...")
-    EP_FMT = H1 + OUTPUT[0] + "/ep-%s"
+# Grabs title and url of episode if requested anime exists
+status("Locating...")
+OUTPUT = findserver(HOST + "/filter?type[]=tv&language[]=subbed&keyword=", ANIME)
+
+
+
+if OUTPUT:  #  Checks if inputted anime exists within host
+    status("Found...")
+    EP_FMT = HOST + OUTPUT[0] + "/ep-%s"
 else:
     quit(input("Unavailable Anime"))
 
 
-if "-" in EPS:
+
+if "-" in EPS:  #  Allows for multiple downloads
     EPS = range(*[num if index < 1 else num + 1 for index, num in enumerate(map(int, EPS.split("-")))])
 
 
-PATH = check_path_title(f"C:\\Users\\evand\\Videos\\Anime\\{OUTPUT[1]}\\")
+
+#  Will implement drive changing feature
+PATH = check_path_title(DOWNLOAD_PATH + "\\" + OUTPUT[1] + "\\")
 
 
-print("Grabbing...")
-INFO = list(grabbing(check_path_ep(EPS if isinstance(EPS, range) else [EPS])))
+
+#  Grabs direct download from host if applicable episode
+status("Grabbing...")
+INFO = list(grabbing(list(check_path_ep(EPS if isinstance(EPS, range) else [EPS]))))
 
 
-print("Downloading...")
+
+#  Downloads each episode to according path
+status("Downloading...")  
 download(INFO)
+
+
+
+
+input("Press ENTER to exit...")
