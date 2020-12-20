37c3d976af745e3263c20e23c70f19de1faaa16f
Evan Schwartzentruber <62043858+Daemitrious@users.noreply.github.com>
2020-12-20 00:51:15 -0500

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,6 +1,8 @@
 from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
 
+from subprocess import Popen
+
 from requests import get, post
 from bs4 import BeautifulSoup
 
@@ -12,26 +14,39 @@ from getpass import getuser
 from time import sleep
 
 
-def headless(options):  #  Set selenium options to headless mode
+
+def headless(options):  #  [Temporary Function] -> Set Options (selenium) to headless mode
     options.add_argument("--headless")
     return options
 
-def status(content, bag=['']):  #  Print over initial print
-    bag.append(content)
-    print(str(content) + " " * len(bag[-2]), end="\r")
+
+
+#  Setup
+Popen("cls", shell=True).wait()
+
+HEADLESS = headless(Options())
+DOWNLOAD_PATH = "C:\\Users\\" + getuser() + "\\Videos\\Anime"
+
+del Popen, getuser, Options, headless
+
 
 
 #  Hosts
-HOST = "https://www12.9anime.to"
+HOST = "https://www12.9anime.to/"
 VIDSTREAM = "https://vidstream.pro/download/"
-
 
 #  Headers required for post request
 HEADERS = {"Content-Type": "application/x-www-form-urlencoded"}
 
 
-def usr(content):  #  Manipulates user's input in order to grab anime and episodes(s)
-    return [x.strip() for x in content.split(",")]
+
+def status(content, bag=['']):  #  Print over initial print
+
+    content += "..."
+    bag.append(content)
+    print(content + " " * len(bag[-2]), end="\r")
+
+
 
 
 def getsoup(html):  #  html  ->  BeautifulSoup
@@ -43,12 +58,32 @@ def gethtml(url):  #  requests.get  ->  BeautifulSoup
 def getjs(driver, urls):  #  webdriver.get  -> BeautifulSoup
 
     for url in urls:
-        status(f"Scraping episode {url.rsplit('-', 1)[1]}...")
+        status(f"Scraping episode {url.rsplit('-', 1)[1]}")
         driver.get(url)
         sleep(1)
         yield getsoup(driver.page_source)
 
     driver.quit()
+
+
+
+def usr(content):  #  Manipulates user's input in order to grab anime and episodes(s)
+    return [x.strip() for x in content.split(",")]
+
+
+def check_path_ep(EPISODES):  #  Checks if any of the episodes requested are already downloaded
+
+    for ep in EPISODES:
+        if not exists(PATH % ep):
+            yield EP_FMT % ep
+
+
+def check_path_title(path):  #  Checks if there is an existing directory of the requested anime
+
+    if not exists(path):
+        makedirs(path)
+
+    return path + "Ep %s.mp4"
 
 
 
@@ -68,25 +103,8 @@ def findserver(url, title):  #  Finds title and server of anime
 
 def grabbing(EPISODES):  #  Grabs path and direct download link for each episode
 
-    for path, dl in zip([PATH % ep.rsplit("-", 1)[1] for ep in EPISODES], [grabdl(VIDSTREAM + grabkey(soup)) for soup in getjs(Firefox(options=headless(Options())), EPISODES)]):
+    for path, dl in zip([PATH % ep.rsplit("-", 1)[1] for ep in EPISODES], [grabdl(VIDSTREAM + grabkey(soup)) for soup in getjs(Firefox(options=HEADLESS), EPISODES)]):
         yield path, dl
-
-
-
-def check_path_ep(EPISODES):  #  Checks if any of the episodes requested are already downloaded
-
-    for ep in EPISODES:
-        if not exists(PATH % ep):
-            yield EP_FMT % ep
-
-
-def check_path_title(path):  #  Checks if there is an existing directory of the requested anime
-
-    if not exists(path):
-        makedirs(path)
-
-    return path + "Ep %s.mp4"
-
 
 
 def download(INFO):  #  Writes the content of each direct download link to the specified path.
@@ -101,50 +119,44 @@ def download(INFO):  #  Writes the content of each direct download link to the s
 
 
 
-
-#  Path to where the anime is stored
-DOWNLOAD_PATH = "C:\\Users\\" + getuser() + "\\Videos\\Anime"
-
+#  Determine anime and requested episodes from user
+ANIME, EPS = usr(input(f'Download Path:  "{DOWNLOAD_PATH}"\n\n\t> '))
 
 
-ANIME, EPS = usr(input(f"{DOWNLOAD_PATH}\n\n\t> "))
+#  For formatting purposes
+print()
 
 
 # Grabs title and url of episode if requested anime exists
-status("Locating...")
-OUTPUT = findserver(HOST + "/filter?type[]=tv&language[]=subbed&keyword=", ANIME)
-
+status("Locating")
+OUTPUT = findserver(HOST + "filter?type[]=tv&language[]=subbed&keyword=", ANIME)
 
 
 if OUTPUT:  #  Checks if inputted anime exists within host
-    status("Found...")
-    EP_FMT = HOST + OUTPUT[0] + "/ep-%s"
+    status("Found")
+    EP_FMT = HOST + OUTPUT[0][1:] + "/ep-%s"
 else:
     quit(input("Unavailable Anime"))
-
 
 
 if "-" in EPS:  #  Allows for multiple downloads
     EPS = range(*[num if index < 1 else num + 1 for index, num in enumerate(map(int, EPS.split("-")))])
 
 
-
 #  Will implement drive changing feature
 PATH = check_path_title(DOWNLOAD_PATH + "\\" + OUTPUT[1] + "\\")
 
 
-
 #  Grabs direct download from host if applicable episode
-status("Grabbing...")
+status("Grabbing")
 INFO = list(grabbing(list(check_path_ep(EPS if isinstance(EPS, range) else [EPS]))))
 
 
-
 #  Downloads each episode to according path
-status("Downloading...")  
+status(f"Beginning Download{'s' if isinstance(EPS, range) else ''}")
 download(INFO)
 
 
 
 
-input("Press ENTER to exit...")
+input("Press ENTER to exit")
