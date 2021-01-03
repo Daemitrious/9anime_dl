52d7cb2815f0da9f42d08170e283ff42e7f12efc
Evan Schwartzentruber <62043858+Daemitrious@users.noreply.github.com>
2021-01-03 06:50:07 -0500

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -23,16 +23,12 @@ from getpass import getuser
 
 
 
-def clear():
+def clear():  #  For formatting purposes
     Popen("cls", shell=True).wait()
 
 
-clear()
-
-
-
-def kill(reason): 
-    exit(input("\n" + reason))
+def kill(reason):  #  In case of uncaught exception
+    exit(ask(f"Press Enter to exit  |  [{reason}]", True))
 
 
 def status(content, static=False, bag=['']):  #  Print over initial print
@@ -45,9 +41,15 @@ def status(content, static=False, bag=['']):  #  Print over initial print
         print(content + " " * len(bag[-2]), end="\r")
 
 
-def ask(content):  #  Formatted for user-input
+def ask(content, before=False):  #  Formatted for user-input
+
+    if before:
+        clear()
+
     output = input(content + "\n\n\t> ")
+
     clear()
+
     return output
 
 
@@ -59,11 +61,11 @@ def driver():  #  Initiate selenium.webdriver
     return Firefox(options=headless(Options()))
 
 
-def ec(xpath):
+def ec(xpath):  #  Premade EC for WebDriverWait
     return expected_conditions.visibility_of_element_located((By.XPATH, xpath))
 
 
-def mkdir_if_exists(path):
+def mkdir_if_not_exists(path):  #  Make directory if "path" doesn't exist
     if not exists(path):
         makedirs(path)
 
@@ -72,10 +74,10 @@ def soupify(html):  #  html  ->  BeautifulSoup
     return BeautifulSoup(html, "lxml")
 
 
-def gethtml(url):  #  requests.get  ->  BeautifulSoup
+def gethtml(url):  #  requests.get
     return soupify(get(url).text)
 
-def getjs(Driver, urls, find):
+def getjs(Driver, urls, find):  #  Driver.get
     Wait = WebDriverWait(Driver, 5, 3)
 
     for url in (urls if isinstance(urls, list) else [urls]):
@@ -96,37 +98,37 @@ def getjs(Driver, urls, find):
 
 
 def show_list(titlelist):  #  Formats results [0-9] for selection
-    return (lambda MAX: 'Search results from "{0}"\n\n{2}\n{1}\n{2}'.format(anime, "\n".join(("%s%s[%i]" % (t, " " * (MAX - len(t)), i) for i, t in enumerate(titlelist))), "-" * (MAX  + 3))) (len(max(titlelist, key=len)) + 5)
+    return (lambda MAX: 'Search results from "{0}"\n\n{2}\n{1}\n{2}'.format(search, "\n".join(("%s%s[%i]" % (t, " " * (MAX - len(t)), i) for i, t in enumerate(titlelist))), "-" * (MAX  + 3))) (len(max(titlelist, key=len)) + 5)
 
-def show_dict(data):
+def show_dict(data):  #  Create dictionary for data
     return {str(i): th for i, th in enumerate(data)}
 
 
-def listsearch(soup):
+def listsearch(soup):  #  Prompt user with results of search and grab title and href based off input
     if soup.find("ul", class_="anime-list").find("li"):
         return (lambda data: show_dict(data).get(ask(show_list([t[0] for t in data])))) ([(a["data-jtitle"], a["href"][1:]) for a in [li.find("a", class_="name") for li in soup.find("ul", class_="anime-list").find_all("li")[:10]]])
     else:
         kill("Anime not found")
 
 
-def sortinput(inp, EPS):  #  Selects applicable episode(s) for download
+def sortinput(inp):  #  Selects applicable episode(s) for download
 
     try:
         if "-" not in inp:
-            if not exists(PATH % inp):
-                return [EPS[EPS.index(inp)]]
+            if not exists(path % inp):
+                return [eps[eps.index(inp)]]
             else:
                 kill("Episode already downloaded")
 
         else:
-            return [ep for ep in EPS[(lambda start, end: slice(start, end + 1)) (*(EPS.index(indice) for indice in inp.split("-")))] if not exists(PATH % ep)]
+            return [ep for ep in eps[(lambda start, end: slice(start, end + 1)) (*(eps.index(indice) for indice in inp.split("-")))] if not exists(path % ep)]
 
     except IndexError:
         kill("Invalid Episode")
 
 
 def grabeps(driver):  #  Grab each episode number from each ul available
-    for ul in list(getjs(driver, EP_FMT.rsplit("/", 1)[0], ec("//ul[@class='episodes']")))[0].find_all("ul", class_="episodes"):
+    for ul in list(getjs(driver, epURL.rsplit("/", 1)[0], ec("//ul[@class='episodes']")))[0].find_all("ul", class_="episodes"):
         for li in ul:
             for a in li:
                 if isinstance(a, Tag):  #  Might need Try/Except (ValueError)
@@ -140,12 +142,12 @@ def grabdl(URL):  #  Post request in order to grab downlink link from direct hos
 
 
 def grabbing(driver, EPISODES):  #  Grabs path and direct download link for each episode
-    return [(path[:-4].rsplit("\\", 1)[1], path, dl) for path, dl in zip([PATH % ep for ep in EPISODES], [grabdl(VIDSTREAM + grabkey(soup)) for soup in getjs(driver, [EP_FMT % ep for ep in EPISODES], ec("//div[@id='player']//iframe"))])]
+    return [(p[:-4].rsplit("\\", 1)[1], p, dl) for p, dl in zip([path % ep for ep in EPISODES], [grabdl(VIDSTREAM + grabkey(soup)) for soup in getjs(driver, [epURL % ep for ep in EPISODES], ec("//div[@id='player']//iframe"))])]
 
 
-def download(INFO):  #  Writes the content of each direct download link to the specified path.
-    for ep, path, url in INFO:
-        with open(path, "wb") as f:
+def download(info):  #  Writes the content of each direct download link to the specified path.
+    for ep, p, url in info:
+        with open(p, "wb") as f:
             status("Downloading " + ep)
 
             for chunk in get(url, stream=True).iter_content(1024):
@@ -159,52 +161,56 @@ MAIN = "https://www12.9anime.to/"
 VIDSTREAM = "https://vidstream.pro/download/"
 
 
-PATH = "C:\\Users\\" + getuser() + "\\Videos\\Anime"
+path = "C:\\Users\\" + getuser() + "\\Videos\\Anime"
 
 
 FILTER = {"\\":"-", "/":"-", ":":" -", "*":"", "?":"" , '"':"'", "<":"-", ">":"-", "|": "-"}
 
 
-anime = ask(f'Download Path:  "{PATH}"')
+#  Anime requested by the user
+search = ask(f'Download Path:  "{path}"', True)
+
 
 # Grabs title and server of anime specified
 status("Locating")
-ANIME, HREF = listsearch(gethtml(MAIN + "search?keyword=" + anime.replace(" ", "+")))
+anime, href = listsearch(gethtml(MAIN + "search?keyword=" + search.replace(" ", "+")))
 
 
 #  Format episode url according to href of anime
 status("Found")
-EP_FMT = MAIN + HREF + "/ep-%s"
+epURL = MAIN + href + "/ep-%s"
 
 
 #  Creates new tree if directory of anime title does not exist
-PATH += f"\\{''.join(map(lambda char: FILTER.get(char) if char in FILTER.keys() else char, ANIME))}\\Episode %s.mp4"
+path += f"\\{''.join(map(lambda char: FILTER.get(char) if char in FILTER.keys() else char, anime))}\\Episode %s.mp4"
 
 
 #  Grabs each valid episode
 status("Grabbing episodes")
-EPS = list(grabeps(driver()))
+eps = list(grabeps(driver()))
 
 
 #  Grabs episodes from EPS list according to user input
-REQUESTED = sortinput(ask(f"Which Episodes ({EPS[0]}-{EPS[-1]}):"), EPS)
+requested = sortinput(ask(f"Which Episodes ({eps[0]}-{eps[-1]}):"))
 
 
 #  For formatting purposes
-PLURAL = "s" if len(REQUESTED) > 1 else ''
+plural, amount = "s" if len(requested) > 1 else '', len(requested)
 
 
 #  Grabs direct download from host if applicable episode
-status(f"Requesting direct link{PLURAL} from host")
-INFO = grabbing(driver(), REQUESTED)
+status(f"Requesting direct link{plural} from host")
+info = grabbing(driver(), requested)
 
 
-mkdir_if_exists(PATH.rsplit("\\", 1)[0])
+#  Create new directory if path is non-existant
+mkdir_if_not_exists(path.rsplit("\\", 1)[0])
 
 
 #  Downloads each episode to according path
-status(f"Requested {len(REQUESTED)} episode{PLURAL} of {ANIME}")
-download(INFO)
+status(f'Requested {amount} episode{plural} of {anime}:\n', True)
+download(info)
 
 
-input("Press ENTER to exit")
+#  End program
+kill(f"Process Finished: Downloaded {amount} episodes")
